f5cca2cefd3bd0b651eef58e126fbb6cd9067024
Simen <simenhol@uia.no>
2023-02-08 19:54:03 +0100

    Oppdatert

--- funfacts/funcfacts_test.go
+++ funfacts/funcfacts_test.go
@@ -14,19 +14,21 @@ import (
 */
 func TestGetFunFacts(t *testing.T) {
 	type test struct {
-		input // her må du skrive riktig type for input
-		want  // her må du skrive riktig type for returverdien
+		input string // her må du skrive riktig type for input
+		want  int    // her må du skrive riktig type for returverdien
 	}
 
-	// Her må du legge inn korrekte testverdier
-	//tests := []test{
-	//  {input: , want: },
-	//}
+	var tests = []test{ // Her må du legge inn korrekte testverdier
+		{Sun, 0}, //tests := []test{
+		{1, 1},   //  {input: , want: },
+		{2, 1},   //}
+		{3, 2},
+	}
 
 	for _, tc := range tests {
 		got := GetFunFacts(tc.input)
 		if !reflect.DeepEqual(tc.want, got) {
-			t.Errorf("expected: %v, got: %v", tc.want, got)
+			t.Errorf("expected: %v, got: %v", tc.want, got) // tc.input,
 		}
 	}
 }
